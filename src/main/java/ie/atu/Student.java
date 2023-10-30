4974533ece60715008427da4412dd8450d17322f
tremain <g00367973@atu.ie>
2023-10-30 12:35:38 +0000

    Added getters and setters

--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -1,9 +1,9 @@
 package ie.atu;
 
 public class Student {
-    String studentName;
-    String studentEmail;
-    String studentCourse;
+    private String studentName;
+    private String studentEmail;
+    private String studentCourse;
 
     //default constructor
     public Student()
@@ -11,6 +11,31 @@ public class Student {
         studentName = "John Doe";
         studentEmail = "John.doe@atu.ie";
         studentCourse = "Business studies";
+    }
+
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
+    }
+
+    public String getStudentCourse() {
+        return studentCourse;
+    }
+
+    public void setStudentCourse(String studentCourse) {
+        this.studentCourse = studentCourse;
     }
 
     public void displayInfo()
