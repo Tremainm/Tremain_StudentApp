477932eb64b036d9558dfe7ade61ac6d90aedaa8
tremain <g00367973@atu.ie>
2023-10-30 11:37:49 +0000

    Added class default constructor

--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -4,4 +4,13 @@ public class Student {
     String studentName;
     String studentEmail;
     String studentCourse;
+
+    //default constructor
+    public Student()
+    {
+        studentName = "John Doe";
+        studentEmail = "John.doe@atu.ie";
+        studentCourse = "Business studies";
+    }
+
 }
